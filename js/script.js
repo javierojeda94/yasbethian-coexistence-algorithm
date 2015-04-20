f7bdc49375908630ad8abb114e8a6ec0913e7bbc
Javier Ojeda Maldonado <Javier Ojeda>
2015-04-19 20:54:37 -0500

    agregado el esqueleto general

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        anchors:['intro','paso1','paso2','paso3','paso4','paso5','paso6','paso7','paso8','paso9','paso10','paso11','paso12','paso13','paso14','paso15','paso16','paso17','paso18','paso19','paso20','creditos'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['intro','paso1','paso2','paso3','paso4','paso5','paso6','paso7','paso8','paso9','paso10','paso11','paso12','paso13','paso14','paso15','paso16','paso17','paso18','paso19','paso20','creditos'],
+        showActiveTooltips: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: false,
+        fitToSection: true,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: true,
+        loopTop: true,
+        loopHorizontal: true,
+        continuousVertical: true,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : false,
+        sectionsColor : ['#ABCDEF','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ccc', '#fff','#ABCDEF'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsive: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
+});
